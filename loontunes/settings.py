756ba0746dabe3721bb5ccf31844e9aef2ec702b
zeke <ezekielpaints@gmail.com>
2022-06-28 13:20:12 -0400

    added allauth

--- loontunes/settings.py
+++ loontunes/settings.py
@@ -30,6 +30,17 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
+AUTHENTICATION_BACKENDS = [
+    ...
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+    ...
+]
+
+
 INSTALLED_APPS = [
     'social',
     'landing',
