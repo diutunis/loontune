a2d6c0b13e8e494620d08453b5a37dec512d0702
zeke <ezekielpaints@gmail.com>
2022-06-29 18:04:00 -0400

    created a posting interface

--- social/views.py
+++ social/views.py
@@ -1,16 +1,19 @@
 from django.shortcuts import render
 from django.views import View
 from .models import Post
+from .forms import PostForm
 
 
 class PostListView(View):
     def get(self, request, *args, **kwargs):
         posts = Post.objects.all().order_by('-created_on')
+        form = PostForm()
 
         context = {
             'post_list':posts,
+            'form': form,
         
         }
 
         return render(request, 'social/post_list.html', context)  
-# Create your views here.
+
