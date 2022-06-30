38283a6ff45422c59e7f0d68fc067965341e762a
zeke <ezekielpaints@gmail.com>
2022-06-30 14:24:41 -0400

    added if statement to homepage redirect

--- loontunes/urls.py
+++ loontunes/urls.py
@@ -22,7 +22,5 @@ urlpatterns = [
 
     path('accounts/', include('allauth.urls')),
 
-    path('social/', include('social.urls')),
-    
-    path('uploads/', include('social.urls'))
+    path('social/', include('social.urls'))
 ]
