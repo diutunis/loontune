450dcc43aca1b27f082729e79a502a6c11f81b9f
zeke <ezekielpaints@gmail.com>
2022-07-01 17:05:36 -0400

    addeduser profile model

--- social/models.py
+++ social/models.py
@@ -1,3 +1,4 @@
+from email.policy import default
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
@@ -7,6 +8,14 @@ from django.dispatch import receiver
 
 class Post(models.Model) :
     body = models.TextField()
-    image= models.ImageField(upload_to='uploads/post_photos', blank=True, null=True)
+    image = models.ImageField(upload_to='uploads/post_photos', blank=True, null=True)
     created_on = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, primary_key=True, verbose_name='user', related_name='profile', on_delete=models.CASCADE)
+    name = models.CharField(max_length=10, blank=True, null=True)
+    bio = models.TextField(max_length=300, blank=True, null=True)
+    location = models.CharField(max_length= 100, blank=True, null=True)
+    picture = models.ImageField(upload_to='uploads/profile_pictures', default= 'uploads/profile_pictures/default.jpeg', blank=True)
+
