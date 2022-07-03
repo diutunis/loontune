7996773fd07dc765407b7a7af8a64974b1d930d6
zeke <ezekielpaints@gmail.com>
2022-07-03 18:16:51 -0400

    working profilesn profileview

--- social/urls.py
+++ social/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path 
-from .views import PostListView, PostDetailView
+from .views import PostListView, PostDetailView, ProfileView
 
 urlpatterns = [
     path('',PostListView.as_view(),name= 'post-list'),
     path('post/<int:pk>', PostDetailView.as_view(), name='post-detail'),
+    path('profile/<int:pk>', ProfileView.as_view(), name='profile'),
     
 ]
