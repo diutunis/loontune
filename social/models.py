4a8a379a70fc7eea2e9e2d2c9b9ae73b9681cc68
zeke <ezekielpaints@gmail.com>
2022-07-07 02:03:00 -0400

    changed api wow we LIVE!

--- social/models.py
+++ social/models.py
@@ -5,19 +5,29 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 import requests
+import random
 
 
 class Post(models.Model) :
+    querystring = {"name":"{body}"}
+    headers = {
+	"X-RapidAPI-Key": "7d6f0e6ddcmsh07cf4610b0f633ap1657a2jsn77b8367a1023",
+	"X-RapidAPI-Host": "spotify-scraper.p.rapidapi.com"
+}
     body = models.CharField(max_length= 6)
     image = models.ImageField(upload_to='uploads/post_photos', blank=True, null=True)
     created_on = models.DateTimeField(default=timezone.now)
+    rand = random.randrange(110000, 112024, 1)
+
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    r = requests.get('http://theaudiodb.com/api/v1/json/523532/mvid.php?i=112020',)
-    song = r.json()['mvids'][0]['strMusicVid']
+    r = requests.get("https://spotify-scraper.p.rapidapi.com/v1/track/search", headers=headers, params=querystring)
+    song = r.json()['shareUrl']
+  
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, primary_key=True, verbose_name='user', related_name='profile', on_delete=models.CASCADE)
     name = models.CharField(max_length=10, blank=True, null=True)
+ 
     bio = models.TextField(max_length=300, blank=True, null=True)
     location = models.CharField(max_length= 100, blank=True, null=True)
     picture = models.ImageField(upload_to='uploads/profile_pictures', default= 'uploads/profile_pictures/default.jpeg', blank=True)
